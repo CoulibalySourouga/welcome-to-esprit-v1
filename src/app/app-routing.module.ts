89156ba149c74c0321b1951d10dd3089d93eba29
CoulibalySourouga <Sourouga.Coulibaly@gmail.com>
2023-04-17 22:11:33 +0100

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageLoginComponent} from "./page-login/page-login.component";
+import {PageInscriptionComponent} from "./page-inscription/page-inscription.component";
+import {PageDashboardComponent} from "./page-dashboard/page-dashboard.component";
+import * as path from "path";
+import {PageStastiquesComponent} from "./page-stastiques/page-stastiques.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'login', component:PageLoginComponent
+  },
+  {
+    path:'inscrire', component:PageInscriptionComponent
+  },
+  {
+    path:'dashboard', component:PageDashboardComponent,
+    children:[
+      {
+        path:'stats', component:PageStastiquesComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageLoginComponent } from './page-login/page-login.component';
+import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
+import { PageStastiquesComponent } from './page-stastiques/page-stastiques.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLoginComponent,
+    PageInscriptionComponent,
+    PageDashboardComponent,
+    PageStastiquesComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
